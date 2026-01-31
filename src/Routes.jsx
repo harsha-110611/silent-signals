85c61874f6ca91c9a41e3898c92e7b3b4eca6e2f
harsha-110611 <harshamalle15@gmail.com>
2026-01-31 15:55:28 +0530

    Fix routing for GitHub Pages using HashRouter

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,15 +1,16 @@
 import React from "react";
-import { BrowserRouter, Routes as RouterRoutes, Route } from "react-router-dom";
+import { HashRouter, Routes as RouterRoutes, Route } from "react-router-dom";
 
 import AuthenticationLogin from './pages/authentication-login';
 
 const Routes = () => {
   return (
-    <BrowserRouter>
-      <RouterRoutes>
-        <Route path="/" element={<AuthenticationLogin />} />
-      </RouterRoutes>
-    </BrowserRouter>
+<HashRouter>
+  <RouterRoutes>
+    <Route path="/" element={<AuthenticationLogin />} />
+  </RouterRoutes>
+</HashRouter>
+
   );
 };
 
